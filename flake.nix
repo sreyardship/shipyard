35740456d21107f2a67a6c7b6984f4211be28707
Jumziey <jumziey@gmail.com>
2023-05-15 22:31:15 +0200

    Added kustomize and helm as repo tools

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs.devshell.url = "github:numtide/devshell";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
 
   outputs = { self, flake-utils, devshell, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system: {
